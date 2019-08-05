dacedc5caa159bee4eca33cd9983ab1fd7f44706
Afshin <mnafshin@gmail.com>
2019-08-05 14:35:57 +0200

    tripleStoreWriter service is added

---
+++ triplestore-writer/src/test/java/org/diceresearch/triplestorewriter/TripleStoreWriterApplicationTests.java
@@ -0,0 +1,16 @@
+package org.diceresearch.triplestorewriter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TripleStoreWriterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
