57414831f11061d17e9c7923fb85512e7e73f58b
Afshin <mnafshin@gmail.com>
2019-09-20 15:00:56 +0200

    production configurations for rabbitmq and elastic search and kibana are set in the docker-compose.yml (which moved from version 2.1 to 3.2)

--- howToRun.sh
+++ howToRun.sh
@@ -1,8 +1,3 @@
-#!/bin/bash
-sudo mkdir /var/log/converter/
-sudo mkdir /var/log/converter1/
-sudo mkdir /var/log/converter2/
-
 #add opal graph to fuseki triple store
 #add exchange ex_logstash to RabbitMQ Exchanges
 #add queue q_logstash to RabbitMQ queues
