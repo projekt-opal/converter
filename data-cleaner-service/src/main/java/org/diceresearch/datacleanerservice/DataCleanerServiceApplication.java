fca1e5e41826ee75eef19e7575ed0628418da5a4
Afshin <mnafshin@gmail.com>
2020-04-08 02:18:27 +0200

    Enhanced: data-cleaner service is added to call CatFish component

--- data-cleaner-service/src/main/java/org/diceresearch/datacleanerservice/DataCleanerServiceApplication.java
+++ data-cleaner-service/src/main/java/org/diceresearch/datacleanerservice/DataCleanerServiceApplication.java
@@ -2,8 +2,14 @@ package org.diceresearch.datacleanerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Processor;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableBinding(Processor.class)
 public class DataCleanerServiceApplication {
 
     public static void main(String[] args) {
