7507508776ed359e86ae426f90ea9dc7c9edca2d
Afshin <mnafshin@gmail.com>
2020-04-08 02:17:33 +0200

    Maintain: DatasetFileFetcher properties are extracted to config server. and logback-spring.xml is added

--- dataset-file-fetcher/src/main/java/org/diceresearch/datasetfilefetcher/DatasetFileFetcherApplication.java
+++ dataset-file-fetcher/src/main/java/org/diceresearch/datasetfilefetcher/DatasetFileFetcherApplication.java
@@ -5,8 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableBinding(Source.class)
 public class DatasetFileFetcherApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
