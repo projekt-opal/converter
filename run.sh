6869f8c80f123fab2fded3ebb9e60b3154b5ef82
Adrian Wilke <adibaba@users.noreply.github.com>
2020-05-28 18:28:09 +0200

    added sudo for docker-compose commands

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 mvn clean install
-docker-compose build
-docker-compose up -d rabbitmq es discovery-server
+sudo docker-compose build
+sudo docker-compose up -d rabbitmq es discovery-server
 echo ==============
 echo please add ex_logstash and bind q_logstash to it
 echo ==============
 echo -n "Press [ENTER] to continue,...: "
 # shellcheck disable=SC2162
 read var_name
-docker-compose up -d config-server kibana logstash
+sudo docker-compose up -d config-server kibana logstash
 sleep 10
-docker-compose up -d
+sudo docker-compose up -d
