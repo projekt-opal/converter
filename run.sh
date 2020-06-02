7d35c3793dacd2d3b2ac970d1e3637353f02aaa2
Adrian Wilke <adibaba@users.noreply.github.com>
2020-06-02 10:21:06 +0200

    optional sudo in run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+
+# Add sudo if not loggin in as root user
+PREFIX=''
+if [ "$EUID" -ne 0 ]
+  then
+    PREFIX='sudo '
+fi
+
 mvn clean install
-sudo docker-compose build
-sudo docker-compose up -d rabbitmq es discovery-server
+eval "${PREFIX}docker-compose build"
+eval "${PREFIX}docker-compose up -d rabbitmq es discovery-server"
 echo ==============
 echo please add ex_logstash and bind q_logstash to it
 echo ==============
 echo -n "Press [ENTER] to continue,...: "
 # shellcheck disable=SC2162
 read var_name
-sudo docker-compose up -d config-server kibana logstash
+eval "${PREFIX}docker-compose up -d config-server kibana logstash"
 sleep 10
-sudo docker-compose up -d
+eval "${PREFIX}docker-compose up -d"
